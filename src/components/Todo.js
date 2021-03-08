7b2a0b06787743c48be1ca6a5545e1edc3dac139
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-08 18:43:27 +0100

    changed current plans

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,8 +5,9 @@ const Todo = () => {
         <div>
             <br></br>
             <h1>What I am currently working on and future plans</h1>
-            <p>I am currently focusing on my chess application, to incorporate a Join Game screen and finally implement castling and an passant. </p>
-            <p>Afterwards I think I want to go through my currently uploaded projects and then rework this portfolio site, giving more information regarding each project. </p>
+            <p>I am currently focusing on my chess application. The game itself (should) works bug-free but I would like to make the code cleaner before adding additional functionality, such as a chat or login system</p>
+            <p>I think I want to go through my currently uploaded projects and then rework this portfolio site, giving more information regarding each project. </p>
+            <p>I want to create an online-version of the board game King of Tokyo</p>
         </div>
     )
 }
