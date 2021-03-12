da59d46ed29e814e10dd65c8d496a1cb49ef96ba
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-12 15:10:12 +0100

    changed text

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ const Todo = () => {
             <br></br>
             <h1>What I am currently working on and future plans </h1><h3>(last update: 2021-03-12)</h3>
             <p>I have just finished the actual chess game part of my chess website. It (should) work bug-free. In the future I would like to make an authentication system as well as incorporate a chat, but for now I think I want to
-                focus on making my portfolio site better, giving a more detailed description for each project.</p>
+                focus on making a more detailed portfolio site, as this version was just something I quickly threw together to show possible employers my projects. </p>
             <p>Other future plans include making a website for a mock-restaurant and projects using Java/C#/Python.</p>
         </div>
     )
